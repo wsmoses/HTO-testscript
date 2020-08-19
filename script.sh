fe4c4ee8273e03e12d0a743b9325b9d5cb8ce0c3
Ubuntu <ubuntu@ip-172-31-44-151.ec2.internal>
2020-08-19 21:57:59 +0000

    upd script

--- script.sh
+++ script.sh
@@ -32,62 +32,72 @@ mkdir $SUITE
 
 git clone git@github.com:wsmoses/HTO-test-suite forheaders -b annotate --depth 1
 cd forheaders
-mkdir build
-cd build
-cmake .. -DSUITEDIR=$SUITE -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseNoLTO.cmake
-make -j$CORES -i
 
 for i in $(seq 1 $REPEAT); do
+	rm -rf build
+	mkdir build
+	cd build
+	cmake .. -DSUITEDIR=$SUITE -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseNoLTO.cmake
+	make -i
     $LIT -v -j 1 -o $RESULTS/plain$i.json ./MultiSource || true;
+	cd ..
 done
-cd ../..
+cd ..
 
 git clone git@github.com:wsmoses/HTO-test-suite hto -b fastbuild --depth 1
 cd hto
-mkdir build
-cd build
-cmake .. -DSUITEDIR=$SUITE -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseNoLTO.cmake
-make -j$CORES -i | tee errs.txt
 
 for i in $(seq 1 $REPEAT); do
+rm -rf build
+	mkdir build
+cd build
+cmake .. -DSUITEDIR=$SUITE -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseNoLTO.cmake
+make -i | tee errs.txt
     $LIT -v -j 1 -o $RESULTS/hto$i.json ./MultiSource || true;
+cd ..
 done
-cd ../..
+cd ..
 
 
 git clone git@github.com:wsmoses/HTO-test-suite thinlto -b lto --depth 1
 cd thinlto
-mkdir build
-cd build
-cmake .. -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseThinLTO.cmake
-make -j$CORES -i
 
 for i in $(seq 1 $REPEAT); do
+rm -rf build
+	mkdir build
+cd build
+cmake .. -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseThinLTO.cmake
+make -i
     $LIT -v -j 1 -o $RESULTS/thinlto$i.json ./MultiSource || true;
+cd ..
 done
-cd ../..
+cd ..
 
 git clone git@github.com:wsmoses/HTO-test-suite fulllto -b lto --depth 1
 cd fulllto
-mkdir build
-cd build
-cmake .. -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseLTO.cmake
-make -j$CORES -i
 
 for i in $(seq 1 $REPEAT); do
+rm -rf build
+	mkdir build
+cd build
+cmake .. -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseLTO.cmake
+make -i
     $LIT -v -j 1 -o $RESULTS/fulllto$i.json ./MultiSource || true;
+cd ..
 done
-cd ../..
+cd ..
 
 git clone git@github.com:wsmoses/HTO-test-suite noheaders -b noannotate --depth 1
 cd noheaders
-mkdir build
+
+for i in $(seq 1 $REPEAT); do
+rm -rf build
+	mkdir build
 cd build
 cmake .. -DSUITEDIR=$SUITE -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -C../cmake/caches/ReleaseNoLTO.cmake
 make -j$CORES -i
-
-for i in $(seq 1 $REPEAT); do
     $LIT -v -j 1 -o $RESULTS/noheaders$i.json ./MultiSource || true;
+cd ..
 done
-cd ../..
+cd ..
 
