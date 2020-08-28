584769dc8d1ffcc58d8c0d1858f166ff18f7633a
Ubuntu <ubuntu@ip-172-31-66-58.ec2.internal>
2020-08-28 17:23:45 +0000

    redo

--- script.sh
+++ script.sh
@@ -3,12 +3,15 @@ set -e
 CORES="${CORES:-24}"
 REPEAT="${REPEAT:-10}"
 
-TESTS="alacconvert-decode alacconvert-encode burg clamscan ldecod lencod SIBsim4 SPASS aha make_dparser hbd hexxagon kc lambda lemon lua minisat Obsequi oggenc sgefa siod spiff sqlite3 treecc viterbi 7zip-benchmark smg2000 AMGmk CrystalMk IRSmk drop3 five11 uudecode uuencode bullet CLAMR HACCKernels HPCCG PENNANT miniFE CoMD PathFinder rsbench SimpleMOC XSBench miniAMR miniGMG fhourstones3.1 fhourstones analyzer distray fourinarow mason neural pcompress2 pifft cfrac espresso gs qbsort testtrie bisect eks main vor iotest trie bintr imp automotive-basicmath automotive-bitcount automotive-susan consumer-jpeg consumer-lame consumer-typeset network-dijkstra network-patricia office-ispell office-stringsearch security-blowfish security-rijndael security-sha telecomm-CRC32 telecomm-fft telecomm-adpcm telecomm-gsm is bh bisort em3d health mst perimeter power treeadd tsp voronoi paq8p np city deriv1 deriv2 employ family fsm garage life objects ocean office primes shapes simul trees vcirc timberwolfmc agrep allroots assembler mybison cdecl compiler fixoutput football gnugo loader simulator unix-smail unix-tbl anagram bc ft ks yacr2 backprop hotspot pathfinder srad scimark2 ControlFlow-dbl ControlFlow-flt ControlLoops-dbl ControlLoops-flt CrossingThresholds-dbl CrossingThresholds-flt Equivalencing-dbl Equivalencing-flt Expansion-dbl Expansion-flt GlobalDataFlow-dbl GlobalDataFlow-flt IndirectAddressing-dbl IndirectAddressing-flt InductionVariable-dbl InductionVariable-flt LinearDependence-dbl LinearDependence-flt LoopRerolling-dbl LoopRerolling-flt LoopRestructuring-dbl LoopRestructuring-flt NodeSplitting-dbl NodeSplitting-flt Packing-dbl Packing-flt Recurrences-dbl Recurrences-flt Reductions-dbl Reductions-flt Searching-dbl Searching-flt StatementReordering-dbl StatementReordering-flt Symbolics-dbl Symbolics-flt enc-3des enc-md5 enc-pc1 enc-rc4 netbench-crc netbench-url 8b10b beamformer bmm dbms ecbdes llu pairlocalalign rawcaudio rawdaudio encode toast cjpeg mpeg2decode nbench sim tramp3d-v4 frame_layout "
+TESTS="alacconvert-decode alacconvert-encode burg clamscan ldecod lencod SIBsim4 SPASS aha make_dparser hbd hexxagon kc lambda lemon lua minisat Obsequi oggenc sgefa siod spiff sqlite3 treecc viterbi 7zip-benchmark smg2000 AMGmk CrystalMk IRSmk drop3 five11 uudecode uuencode bullet CLAMR HACCKernels HPCCG PENNANT miniFE CoMD PathFinder rsbench SimpleMOC XSBench miniAMR miniGMG fhourstones3.1 fhourstones analyzer distray fourinarow mason neural pcompress2 pifft cfrac espresso gs qbsort testtrie bisect eks main vor iotest trie bintr imp automotive-basicmath automotive-bitcount automotive-susan consumer-jpeg consumer-lame consumer-typeset network-dijkstra network-patricia office-ispell office-stringsearch security-blowfish security-rijndael security-sha telecomm-CRC32 telecomm-fft telecomm-adpcm telecomm-gsm is bh bisort em3d health mst perimeter power treeadd tsp voronoi paq8p np city deriv1 deriv2 employ family fsm garage life objects ocean office primes shapes simul trees vcirc timberwolfmc agrep allroots assembler mybison cdecl compiler fixoutput football gnugo loader simulator unix-smail unix-tbl anagram bc ft ks yacr2 backprop hotspot pathfinder srad ControlFlow-dbl ControlFlow-flt ControlLoops-dbl ControlLoops-flt CrossingThresholds-dbl CrossingThresholds-flt Equivalencing-dbl Equivalencing-flt Expansion-dbl Expansion-flt GlobalDataFlow-dbl GlobalDataFlow-flt IndirectAddressing-dbl IndirectAddressing-flt InductionVariable-dbl InductionVariable-flt LinearDependence-dbl LinearDependence-flt LoopRerolling-dbl LoopRerolling-flt LoopRestructuring-dbl LoopRestructuring-flt NodeSplitting-dbl NodeSplitting-flt Packing-dbl Packing-flt Recurrences-dbl Recurrences-flt Reductions-dbl Reductions-flt Searching-dbl Searching-flt StatementReordering-dbl StatementReordering-flt Symbolics-dbl Symbolics-flt enc-3des enc-md5 enc-pc1 enc-rc4 netbench-crc netbench-url 8b10b beamformer bmm dbms ecbdes llu pairlocalalign rawcaudio rawdaudio encode toast cjpeg mpeg2decode nbench sim tramp3d-v4 frame_layout "
+NOTEST="scimark2"
 EXTRA=""
 
+rm -rf results
 mkdir results
 RESULTS="`pwd`/results"
 
+if false; then
 git clone --depth 1 git://sourceware.org/git/binutils-gdb.git binutils
 git clone https://github.com/wsmoses/LLVM-HTO -b rebase2 --depth 1
 #git clone https://github.com/wsmoses/LLVM-HTO -b manglecpp --depth 1
@@ -32,13 +35,15 @@ mkdir build
 cd build
 cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_USE_LINKER=gold -DLLVM_BINUTILS_INCDIR="$BININC"
 make -j$CORES
-export CC="`pwd`/bin/clang"
-export CXX="`pwd`/bin/clang++"
-LIT="`pwd`/bin/llvm-lit"
 cd ../..
 
-SUITE="`pwd`/suite"
+fi
 
+export CC="`pwd`/LLVM-HTO/build/bin/clang"
+export CXX="`pwd`/LLVM-HTO/build/bin/clang++"
+LIT="`pwd`/LLVM-HTO/build/bin/llvm-lit"
+SUITE="`pwd`/suite"
+rm -rf $SUITE
 mkdir $SUITE
 
 cd forheaders
